b2cd70a1a98ab4c75c21080b3b3bee6d22984d85
xichengzhang <zhangxicheng@lizi-inc.com>
2015-12-25 18:31:08 +0800

    aa

---
+++ night/src/main/java/com/good/night/controllers/HelloController.java
@@ -0,0 +1,12 @@
+package com.good.night.controllers;
+
+import net.paoding.rose.web.annotation.Path;
+import net.paoding.rose.web.annotation.rest.Get;
+
+@Path("/hello/")
+public class HelloController {
+	@Get("world")
+	public String index() {
+		return "@hello world";
+	}
+}
