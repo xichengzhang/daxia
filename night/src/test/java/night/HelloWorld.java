db93d75dee862e5232c67cee61837d5ddc5f62e2
xichengzhang <zhangxicheng@lizi-inc.com>
2015-12-27 15:18:17 +0800

    add
    Signed-off-by: xichengzhang <zhangxicheng@lizi-inc.com>

---
+++ night/src/test/java/night/HelloWorld.java
@@ -0,0 +1,14 @@
+package night;
+
+
+public class HelloWorld {
+	
+	public String sayHello(){
+		return "Hello night";
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(new HelloWorld().sayHello());
+	}
+
+}
---
+++ night/src/test/java/night/HelloWorldTest.java
@@ -0,0 +1,18 @@
+package night;
+
+import junit.framework.Assert;
+import org.junit.Test;
+
+public class HelloWorldTest {
+
+	@Test
+	public void test(){
+		
+		HelloWorld helloWorld = new HelloWorld();
+		
+		String result = helloWorld.sayHello();
+		
+		System.out.println(result);
+	}
+	
+}
