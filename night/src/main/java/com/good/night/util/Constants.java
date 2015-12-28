252bdb8d88f5064a8c3004c653539f14cb160167
xichengzhang <zhangxicheng@lizi-inc.com>
2015-12-28 10:29:03 +0800

    add

---
+++ night/src/main/java/com/good/night/util/Constants.java
@@ -0,0 +1,21 @@
+/**
+ *
+ */
+package com.good.night.util;
+
+
+/**
+ * @Description:com.good.night.util.Constants 常量定义类
+ * @author zhangxc  email: zhangxicheng@lizi-inc.com
+ * @date 2015年12月28日 上午10:33:17
+ */
+public class Constants {
+	
+	public static final int BUZ_VIP_NO = 0; // 不是VIP会员
+	public static final int BUZ_VIP_YES = 1; // 是VIP会员
+	public static final int TIME_SMSCODE = 5 * 60; // 短信验证码有效时间,5分钟
+	public static final String MSG_PARAMERROR = "参数错误";
+	public static final String MSG_NOTFOUND = "数据不存在";
+	public static final String MSG_EXISTS = "数据已存在";
+	
+}
